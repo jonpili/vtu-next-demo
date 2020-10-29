b4317fe30cb3423ddd46199a119edfb5671a4e2e
jonpili <jonpili99@gmail.com>
2020-10-29 16:07:07 +0900

    テストを記載

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -4,21 +4,25 @@ import Hello from './Hello.vue'
 
 test('uses mounts', async () => {
   const wrapper = mount(App)
-  expect(wrapper.html()).toContain('Vue app')
-  expect(wrapper.html()).toContain('Hello world')
-  expect(wrapper.html()).toContain('Count: 0')
+  expect(wrapper.html()).toContain('Vue 3 + TypeScript + Jest')
+  expect(wrapper.html()).toContain('smartcamp')
+  expect(wrapper.html()).toContain(100)
+  expect(wrapper.html()).toContain(0.1)
+  expect(wrapper.html()).toContain('Message: ')
 
   await wrapper.find('button').trigger('click')
-  expect(wrapper.html()).toContain('Count: 1')
+  expect(wrapper.html()).toContain('Message: わーい')
 })
 
 test('uses shallowMount', async () => {
   const wrapper = shallowMount(App)
-  expect(wrapper.html()).toContain('Vue app')
-  expect(wrapper.html()).not.toContain('Hello world')
-  expect(wrapper.html()).toContain('Count: 0')
+  expect(wrapper.html()).toContain('Vue 3 + TypeScript + Jest')
+  expect(wrapper.html()).not.toContain('smartcamp')
+  expect(wrapper.html()).not.toContain(100)
+  expect(wrapper.html()).not.toContain(0.1)
+  expect(wrapper.html()).toContain('Message: ')
 
-  // @ts-ignore TODO: Improve types for `findComponent`
-  await wrapper.findComponent(Hello).vm.$emit('greet')
-  expect(wrapper.html()).toContain('Count: 1')
+  // @ts-ignore
+  await wrapper.findComponent(Hello).vm.$emit('say')
+  expect(wrapper.html()).toContain('Message: わーい')
 })
